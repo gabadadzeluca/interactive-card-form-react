69f471d8adee99211f03022e5213393fa962aba3
Luca <gabadadzeluca@gmail.com>
2023-03-31 13:56:46 +0400

    add new validation using react-input-mask module

--- src/components/form/FormComponent.tsx
+++ src/components/form/FormComponent.tsx
@@ -1,4 +1,5 @@
-import Input from "./input/Input";
+import { validateHeaderName } from 'http';
+import InputMask from 'react-input-mask';
 
 export default function FormComponent(props:any){
   const nameData = {
@@ -42,46 +43,69 @@ export default function FormComponent(props:any){
   } = props;
 
 
-  const handleNameChange = (value: string) => {
+  const handleNameChange = (value: string):void => {
     value = value.replace(/[^a-zA-Z\s]/g, '');
     setCardName(value);
   }
 
-  const handleNumber = (value:string) =>{
-    // value = value.replace(/\s/g, "");
-    value = (value.replace(/[^0-9]+/g, ""));
-    if(value.length > 16){
-      return;
-    } else if (value.length > 4 && value.length < 8) {
-      value = value.replace(/(.{4})/g, "$1 ");
-    } else if (value.length > 8 && value.length < 12) {
-      value = value.replace(/(.{4})(.{4})/g, "$1 $2 ");
-    } else if (value.length > 12 && value.length < 16) {
-      value = value.replace(/(.{4})(.{4})(.{4})/g, "$1 $2 $3 ");
-    } else if (value.length == 16) {
-      value = value.replace(/(.{4})(.{4})(.{4})(.{4})/g, "$1 $2 $3 $4");
-    }
-    setCardNum(value);
+  const handleNumber = (value:string):void => {
+    setCardNum(value.replace(/[^0-9]+/g, ""));
   }
 
+  const handleMonth = (value:string):void => {
+    setExpMonth(value.replace(/1[3-9]|[2-9][0-9]/, "12").substring(0, 2));
+  };
+
+  const handleYear = (value: string):void => {
+    const currentYear = new Date().getFullYear().toString().substring(2);
+    if(parseInt(value[0]) < parseInt(currentYear[0])) return;
+    else if(parseInt(value) < parseInt(currentYear)) return;
+    setExpYear(value.substring(0,2));
+  }
 
 
   return(
     <div>
-      <Input 
+      <label>{nameData.cardName.name}</label>
+      <input
+        maxLength={25}
         name={nameData.cardName.name}
         placeholder={nameData.cardName.placeholder}
         type={'text'}
         value={cardName}
         onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{handleNameChange(e.target.value)}}
       />
-      <Input 
+
+      <label>{nameData.cardNum.name}</label>
+
+      <InputMask
+        mask="9999 9999 9999 9999" 
         type={'text'}
+        maskChar={''}
         name={nameData.cardNum.name}
         placeholder={nameData.cardNum.placeholder}
         value={cardNum}
         onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{handleNumber(e.target.value)}}
       />
+      <div>
+        <label>{nameData.expDate.name}</label>
+        <InputMask 
+          mask="99"
+          maskChar={''}
+          value={expMonth}
+          onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleMonth(e.target.value)}
+          id='month'
+          placeholder={nameData.expDate.placeholder1}
+        />
+        <InputMask 
+          mask="99"
+          maskChar={''}
+          value={expYear}
+          onChange={(e:React.ChangeEvent<HTMLInputElement>)=>handleYear(e.target.value)}
+          id="year"
+          placeholder={nameData.expDate.placeholder2}
+        />
+      </div>
     </div>
   )
 }
