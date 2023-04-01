892ae672ef483e48a5b20a028716bdc2031e32c0
Luca <gabadadzeluca@gmail.com>
2023-04-01 14:30:09 +0400

    add state variables and render logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import CardContainerComponent from "./components/card/CardContainerComponent";
+import FormComponent from "./components/form/FormComponent";
 
 function App() {
+  
+  const[expMonth, setExpMonth] = useState<string>('');
+  const[expYear, setExpYear] = useState<string>('');
+  const[cardNum, setCardNum] = useState<string>('');
+  const[cardName, setCardName] = useState<string>('');
+  const[cvc, setCvc] = useState<string>('');
+  const[showError, setShowError] = useState<boolean>(false);
+  const[submitted, setSubmitted] = useState<boolean>(false);
+
+  const formData = {
+    expMonth,
+    expYear,
+    cardNum,
+    cardName,
+    cvc,
+    showError,
+    submitted,
+    setExpMonth,
+    setExpYear,
+    setCardName,
+    setCardNum,
+    setCvc,
+    setShowError,
+    setSubmitted
+  };
+  const displayData = {
+    expMonth,
+    expYear,
+    cardNum,
+    cardName,
+    cvc,
+    showError,
+  }
+
+  console.log("submitted:", submitted);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <CardContainerComponent {...displayData}/> 
+        {submitted ? 
+          (<p>FORM WAS SUBMITTED</p>)
+          :
+          (<FormComponent {...formData}/>)
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
